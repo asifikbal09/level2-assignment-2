9c535d716e61f9c435aa4b00ef5dedb6435b7272
asifikbal09 <asif.ikbal662@gmail.com>
2023-11-23 00:50:02 +0600

    [DONE] Basic projec setup.

---
+++ src/app/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
+
+export default {
+  port: process.env.PORT,
+  mongodb_url: process.env.MONGODB_URL,
+  bcryptSaltRound: process.env.BCRYPT_SALT_ROUND,
+};
